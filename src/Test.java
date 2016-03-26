2369c97a57b7d05f0ba97da8294a18cdf057e5a7
Nadia <nadiiabolibok@gmail.com>
2016-03-26 22:35:05 +0100

    Test N3

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,12 @@
+import java.util.Scanner;
+
 
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method st
 		System.out.println("Hello Githab!");
+		Scanner sc = new Scanner(System.in);
 
 	}
 
